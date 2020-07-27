82002b875e206990bd11f1268e8c3b5456c43bf1
pablo mujica <pablom@pablos-MacBook-Pro.local>
2020-07-26 22:15:38 -0400

    Added bare bones front-end

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+require('./models/UrlShorten')
 const bodyParser = require("body-parser");
 const app = express();
+const mongoose = require("mongoose");
+const UrlShorten = mongoose.model("UrlShorten");
+const path = require('path');
 const PORT = 7000;
 
 //connecting app to mongo
 const mongoURI = "mongodb://localhost/url-shortner";
-const mongoose = require("mongoose");
 const connectOptions = {
     keepAlive: true,
     reconnectTries: Number.MAX_VALUE
@@ -21,6 +24,13 @@ console.log(`Connected to MongoDB`);
 
 //Start server on Port 7000
 app.use(bodyParser.json());
-app.listen(PORT, () => {
+
+app.get('/', (req, res) => {
+    const htmlPath = path.join(__dirname, 'public', 'index.html');
+    res.sendFile(htmlPath);
+  })
+
+app.listen(PORT || 5000, () => {
  console.log(`Server started on port`, PORT);
 });
+
